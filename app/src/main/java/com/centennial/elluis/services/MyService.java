8e0f5a5259fd3a753ad1d996132de4368412a914
Luis Sanchez <lsanch14@my.centennialcollege.ca>
2018-04-18 19:44:02 -0400

    Asynchronous tasks

--- app/src/main/java/com/centennial/elluis/services/MyService.java
+++ app/src/main/java/com/centennial/elluis/services/MyService.java
@@ -2,8 +2,10 @@ package com.centennial.elluis.services;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.net.MalformedURLException;
@@ -17,7 +19,9 @@ public class MyService extends Service {
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-// We want this service to continue running until it is explicitly
+
+        //Running multiple apps
+        /*// We want this service to continue running until it is explicitly
 // stopped, so return sticky.
         // We want this service to continue running until it is explicitly
 // stopped, so return sticky.
@@ -44,9 +48,64 @@ public class MyService extends Service {
 //---return an arbitrary number representing
 // the size of the file downloaded---
         return 100;
-        /*Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
-        return START_STICKY; */
+        *//*Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        return START_STICKY; *//*
+    }*/
+
+        //Performing Tasks in a Service Asynchronously
+        // We want this service to continue running until it is explicitly
+// stopped, so return sticky.
+//Toast.makeText(this, "Service Started", Toast.LENGTH_LONG).show();
+        try {
+            new DoBackgroundTask().execute(
+                    new URL("http://www.amazon.com/somefiles.pdf"),
+                    new URL("http://www.wrox.com/somefiles.pdf"),
+                    new URL("http://www.google.com/somefiles.pdf"),
+                    new URL("http://www.learn2develop.net/somefiles.pdf"));
+        } catch (MalformedURLException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return START_STICKY;
     }
+    private int DownloadFile(URL url) {
+        try {
+            //---simulate taking some time to download a file---
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+//---return an arbitrary number representing
+// the size of the file downloaded---
+        return 100;
+    }
+    private class DoBackgroundTask extends AsyncTask<URL, Integer, Long> {
+        protected Long doInBackground(URL... urls) {
+            int count = urls.length;
+            long totalBytesDownloaded = 0;
+            for (int i = 0; i < count; i++) {
+                totalBytesDownloaded += DownloadFile(urls[i]);
+//---calculate percentage downloaded and
+// report its progress---
+                publishProgress((int) (((i+1) / (float) count) * 100));
+            }
+            return totalBytesDownloaded;
+        }
+        protected void onProgressUpdate(Integer... progress) {
+            Log.d("Downloading files",
+                    String.valueOf(progress[0]) + "% downloaded");
+            Toast.makeText(getBaseContext(),
+                    String.valueOf(progress[0]) + "% downloaded",
+                    Toast.LENGTH_LONG).show();
+        }
+        protected void onPostExecute(Long result) {
+            Toast.makeText(getBaseContext(),
+                    "Downloaded " + result + " bytes",
+                    Toast.LENGTH_LONG).show();
+            stopSelf();
+        }
+    }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
